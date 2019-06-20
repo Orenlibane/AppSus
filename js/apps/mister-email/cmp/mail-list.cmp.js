00b3dfa9e47a8b6f5a198b90345d1b6beae79354
orenTz <orenlibane@gmail.com>
2019-06-20 12:59:58 +0300

    added bin icon for delete

--- js/apps/mister-email/cmp/mail-list.cmp.js
+++ js/apps/mister-email/cmp/mail-list.cmp.js
@@ -6,8 +6,8 @@ export default {
     <section class="mail-list"> 
 
     <ul>
-        <li v-for= "email in emails">
-           <button>Check</button>  <button>Star</button>  "user"  <span>{{email.subject}}</span> <span>{{email.sendAt}}</span>
+        <li v-for= "(email,idx) in emails" :key="idx" class="flex space-between">
+           <span class="main-mail-spec"><button>Check</button>  <button>Star</button>  {{email.name}}</span>  <span class="left subject">{{email.subject}}</span> <span>{{email.sendAt}}</span><i class="fas fa-trash-alt"></i>
         </li>
     </ul>
 

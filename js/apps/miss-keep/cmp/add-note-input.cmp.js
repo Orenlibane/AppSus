7544de7808ca84d8184fb96384b39e634a0e4109
orenTz <orenlibane@gmail.com>
2019-06-22 18:49:54 +0300

    todo modal

--- js/apps/miss-keep/cmp/add-note-input.cmp.js
+++ js/apps/miss-keep/cmp/add-note-input.cmp.js
@@ -3,12 +3,15 @@ export default {
   template: `
     
     <section class="input-container "> 
-        
+
+    
+
+                
                 <input  v-model="newNote.content" v-if="noteTxt" type="text" placeholder="Please enter a text"/>
                 <input   v-model="newNote.picture" v-if="noteImg" type="text" placeholder="Enter Image URL"/>
                 <input   v-model="newNote.video" v-if="noteVideo" type="text" placeholder="Enter Video URL"/>
                 <input   v-model="audio" v-if="noteAudio" type="text" placeholder="Enter Audio"/>
-                <input   v-model="newNote.todos" v-if="noteTodos" type="text" placeholder="Enter Comma Seperated List"/>
+                <input   v-if="noteTodos" type="text" placeholder="Enter Comma Seperated List"/>
                     
                     <i @click="changeNoteState('txt')"   class="fas fa-font"></i> 
                     <i @click="changeNoteState('img')"   class="fas fa-image"></i>
@@ -16,6 +19,16 @@ export default {
                     <i @click="changeNoteState('audio')" class="fas fa-volume-up"></i>    
                     <i @click="changeNoteState('todos')" class="fas fa-list"></i>    
                     <button @click="emitNoteValue">ADD ME</button>
+
+              
+                    <transition name="slide-fade">
+    <div v-if='todoModal' class="  todo-model flex both-align-center column space-between">
+            <h1>TODOS</h1>
+                    <input type="text" v-model="newNote.todos.todo1" placeholder="ENTER YOUR TODO">
+                    <input type="text" v-model="newNote.todos.todo2" placeholder="ENTER YOUR TODO">
+                    <input type="text" v-model="newNote.todos.todo3" placeholder="ENTER YOUR TODO">
+                </div>
+      <transition>
         </section>
 `,
   props: [],
@@ -33,9 +46,14 @@ export default {
         color: null,
         picture: null,
         video: null,
-        todos: null,
-        audio: null
-      }
+        audio: null,
+        todos: {
+          todo1: '',
+          todo2: '',
+          todo3: ''
+        }
+      },
+      todoModal: false
     };
   },
   created() {
@@ -73,6 +91,7 @@ export default {
       if (state === 'todos') {
         this.noteTodos = true;
         this.newNote.type = 'noteToods';
+        this.todoModal = !this.todoModal;
       }
     },
     emitNoteValue() {

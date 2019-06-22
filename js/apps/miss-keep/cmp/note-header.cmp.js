e689ca5dec39e30d2e2d23bd76e6a1c2f7d2d9b1
orenTz <orenlibane@gmail.com>
2019-06-22 14:11:48 +0300

    add notes featrue - middle

--- js/apps/miss-keep/cmp/note-header.cmp.js
+++ js/apps/miss-keep/cmp/note-header.cmp.js
@@ -1,20 +1,20 @@
-import noteFilter from './note-filter.cmp.js';
+// import noteFilter from './note-filter.cmp.js';
 
-export default {
-  name: 'Noteheader',
-  template: `
-    <section class="noteheader flex column align-center flex-end">
-THIS IS THE HEADER
-        <note-filter></note-filter>
-    </section>
-`,
-  // props: [],
-  data() {
-    return {};
-  },
-  created() {},
-  destroyed() {},
-  computed: {},
-  methods: {},
-  components: { noteFilter }
-};
+// export default {
+//   name: 'Noteheader',
+//   template: `
+//     <section class="noteheader flex column align-center flex-end">
+// THIS IS THE HEADER
+//         <note-filter></note-filter>
+//     </section>
+// `,
+//   // props: [],
+//   data() {
+//     return {};
+//   },
+//   created() {},
+//   destroyed() {},
+//   computed: {},
+//   methods: {},
+//   components: { noteFilter }
+// };
--- js/apps/miss-keep/cmp/note-list.cmp.js
+++ js/apps/miss-keep/cmp/note-list.cmp.js
@@ -1,24 +1,22 @@
 import noteService from '../services/noteservice.js';
 import notePrev from './note-prev.cmp.js';
+import noteAdd from './add-note-input.cmp.js';
 
 export default {
   name: 'NoteList',
   template: `
     <section class=note-list> 
     <div class="add-note center flex both-align-center">  
-      <div className="input-container "> 
-  <input type="text" placeholder="What's on your mind?"/>
-   <i class="fas fa-font"></i> <i class="fas fa-image"></i><i class="fab fa-youtube"></i><i class="fas fa-volume-up"></i>
-   </div>
-   </div>
+      <note-add @noteValue="addNote"></note-add>                     
+    </div>
    <div class="no-pinned-notes flex flex-wrap both-align-center">
       <note-prev 
         v-for="(note,idx) in notes"
         :key="idx"
         :note="note" >
       </note-prev>
-        
       </div>
+
     
     </section>
 `,
@@ -34,6 +32,11 @@ export default {
   },
   destroyed() {},
   computed: {},
-  methods: {},
-  components: { notePrev }
+  methods: {
+    addNote(newNote) {
+      console.log(newNote);
+      this.notes.unshift(newNote);
+    }
+  },
+  components: { notePrev, noteAdd }
 };

a419c920d4a8389a05b8e6abb031fc7f50889cfe
orenTz <orenlibane@gmail.com>
2019-06-23 09:27:30 +0300

    check

--- js/main-cmp/header.cmp.js
+++ js/main-cmp/header.cmp.js
@@ -20,10 +20,10 @@ export default {
               <div class="appsus-nav flex both-align-center" v-if="navOpen"> 
              
                 <ul class="flex flex-wrap both-align-center">
-                  <li><router-link to="/"> <img  src="=../../img/house.svg" /> </router-link></li>
-                  <li><router-link to="/misterEmail"> <img  src="../../img/gmail.svg"/> </router-link></li>
-                  <li><router-link to="/missKeep"> <img  src="../../img/notepad.svg"/> </router-link></li>
-                  <li><router-link to="/"> <img  src="../../img/books.svg" /> </router-link></li>
+                  <li><router-link to="/"> <img  src="/../../img/house.svg" /> </router-link></li>
+                  <li><router-link to="/misterEmail"> <img  src="/../../img/gmail.svg"/> </router-link></li>
+                  <li><router-link to="/missKeep"> <img  src="/../../img/notepad.svg"/> </router-link></li>
+                  <li><router-link to="/"> <img  src="/../../img/books.svg" /> </router-link></li>
                 </ul>
                 
               </div>

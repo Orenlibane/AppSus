1251f851872a742f2cb6f5fdb966e5093edd7d6d
orenTz <orenlibane@gmail.com>
2019-06-24 02:25:32 +0300

    footer and some fixings

--- js/main-cmp/footer.cmp.js
+++ js/main-cmp/footer.cmp.js
@@ -2,7 +2,22 @@ export default {
   name: 'Footer-cmp',
   template: `
     <section class="footer-cmp flex both-align-center"> 
-        AppSus - all rights received yada yada
+        <div class="user flex">
+        <div class="user-fb flex both-align-center"><img src="img/oren.svg" /></div>
+              <div class="user-fb flex both-align-center"><i class="fab fa-facebook"></i></div>
+              <div class="user-fb flex both-align-center"><i class="fab fa-github"></i></div>
+                
+        </div>
+        <div class="user inforamtion flex both-align-center">
+        
+We are just two developers on our way to conquer the world! go see our works at github, or know us better in facebook                
+        </div>
+        <div class="user flex">
+        <div class="user-fb flex both-align-center"><img src="img/lior.svg" /></div>
+              <div class="user-fb flex both-align-center"><i class="fab fa-facebook"></i></div>
+              <div class="user-fb flex both-align-center"><i class="fab fa-github"></i></div>
+                
+        </div>
     </section>
 `,
   props: [],

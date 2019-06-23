eb2bb25591ddedb2a95f0b60d416d2737dd223e6
orenTz <orenlibane@gmail.com>
2019-06-24 02:59:42 +0300

    added imgs to libary, and some more styling

--- js/main-cmp/footer.cmp.js
+++ js/main-cmp/footer.cmp.js
@@ -3,7 +3,7 @@ export default {
   template: `
     <section class="footer-cmp flex both-align-center"> 
         <div class="user flex">
-        <div class="user-fb flex both-align-center"><img src="img/oren.svg" /></div>
+        <div class="user-fb flex column both-align-center">Oren Tzezana<img src="img/oren.svg" /></div>
               <div class="user-fb flex both-align-center"><i class="fab fa-facebook"></i></div>
               <div class="user-fb flex both-align-center"><i class="fab fa-github"></i></div>
                 
@@ -13,7 +13,7 @@ export default {
 We are just two developers on our way to conquer the world! go see our works at github, or know us better in facebook                
         </div>
         <div class="user flex">
-        <div class="user-fb flex both-align-center"><img src="img/lior.svg" /></div>
+        <div class="user-fb flex both-align-center column">Lior Lila<img src="img/lior.svg" /></div>
               <div class="user-fb flex both-align-center"><i class="fab fa-facebook"></i></div>
               <div class="user-fb flex both-align-center"><i class="fab fa-github"></i></div>
                 
